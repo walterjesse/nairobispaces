2720fe791f73bb27a1285084126016d8214cc1bc
Walter Jesse <walterjesse@example.com>
2026-04-19 00:43:42 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/nairobi-spaces/",
+  base: "/nairobispaces/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
