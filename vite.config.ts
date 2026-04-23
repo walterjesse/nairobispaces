1b9bcfbf76a9df80642ac5266e31a4accfabda6c
Walter Jesse <walterjesse@example.com>
2026-04-23 14:12:18 +0300

    Change base path to root for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/nairobispaces/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
